96e143de7ad405d4729165b61b198091744fad76
Dmitry Mishakov <welzevul0@gmail.com>
2022-01-11 23:58:56 +0300

    Make Car class is Entity

--- src/main/java/hiber/model/Car.java
+++ src/main/java/hiber/model/Car.java
@@ -1,4 +1,51 @@
 package hiber.model;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
 public class Car {
+
+    @Id
+    @GeneratedValue
+    private Long carId;
+
+    private String model;
+
+    private Integer series;
+
+    public Car() {
+    }
+
+    public Car(String model, Integer series) {
+        this.model = model;
+        this.series = series;
+    }
+
+    public Long getCarId() {
+        return carId;
+    }
+
+    public void setCarId(Long carId) {
+        this.carId = carId;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public Integer getSeries() {
+        return series;
+    }
+
+    public void setSeries(Integer series) {
+        this.series = series;
+    }
+
+
 }
