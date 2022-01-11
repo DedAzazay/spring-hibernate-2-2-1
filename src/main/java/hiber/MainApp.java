a54d3e72354f1bcf10ea1523b3dfb236a5902d20
Dmitry Mishakov <welzevul0@gmail.com>
2022-01-12 02:20:35 +0300

    Added in MainApp tasks by homework

--- src/main/java/hiber/MainApp.java
+++ src/main/java/hiber/MainApp.java
@@ -1,6 +1,7 @@
 package hiber;
 
 import hiber.config.AppConfig;
+import hiber.model.Car;
 import hiber.model.User;
 import hiber.service.UserService;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
@@ -15,10 +16,15 @@ public class MainApp {
 
       UserService userService = context.getBean(UserService.class);
 
-      userService.add(new User("User1", "Lastname1", "user1@mail.ru"));
-      userService.add(new User("User2", "Lastname2", "user2@mail.ru"));
-      userService.add(new User("User3", "Lastname3", "user3@mail.ru"));
-      userService.add(new User("User4", "Lastname4", "user4@mail.ru"));
+      for (int i = 1; i < 5; i++) {
+         User user = new User("User" + i, "Lastname" + i,
+                 "user" + i + "@mail.ru");
+         Car car = new Car("Model" + i, i * 28);
+         user.setCar(car);
+         userService.add(user);
+      }
+
+
 
       List<User> users = userService.listUsers();
       for (User user : users) {
@@ -26,8 +32,19 @@ public class MainApp {
          System.out.println("First Name = "+user.getFirstName());
          System.out.println("Last Name = "+user.getLastName());
          System.out.println("Email = "+user.getEmail());
+         System.out.println("-----CAR------");
+         System.out.println(("Car =[model ="+user.getCar().getModel())+
+                 "; series =" + user.getCar().getSeries() + "]");
          System.out.println();
       }
+
+      User userByCar = userService.getUserByCar("model3", 84);
+      System.out.println("User by car");
+      System.out.println("Id = "+userByCar.getId());
+      System.out.println("First Name = "+userByCar.getFirstName());
+      System.out.println("Last Name = "+userByCar.getLastName());
+      System.out.println("Email = "+userByCar.getEmail());
+
 
       context.close();
    }
